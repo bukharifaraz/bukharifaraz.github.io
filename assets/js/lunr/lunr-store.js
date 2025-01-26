9fa4d86e5c5d4ffdd5e8980393c9b5d3c1d89c78
bukharifaraz <bukharifaraz@users.noreply.github.com>
2025-01-26 12:50:27 +0000

    deploy: 5eb5b8a142be9ad0438292d23df373a5b72579e6

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "जिधर जाने के बाद",
+        "excerpt":"जाओ हमको याद रखना तुम मगर जाने के बाद !! यूँ न हो सब भूल जाओ बन-संवर जाने के बाद !! . हम अभी तो क्या करें तुमसे बहस, ख़ुद देखना ; इन सितारों की सियाही तुम शरर जाने के बाद !! . ताक़ पर जो रख दिया था इक...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/Jidhar-jaane-ke-baad/",
+        "teaser": null
+      },{
+        "title": "List of my Ghazal(s)",
+        "excerpt":"You can find here the list of my Ghazal(s).   अच्छा लगा   Check out the Jekyll docs for more info on how to get the most out of Jekyll. File all bugs/feature requests at Jekyll’s GitHub repo. If you have questions, you can ask them on Jekyll Talk.   ","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/Ghazal/",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/welcome-to-jekyll/",
+        "teaser": null
+      },{
+        "title": "हम नहीं मानते",
+        "excerpt":"Ek had tak bura ham nahiN maante ,  Phir koi ilteja ham nahiN maante.   Ek shayar hai accha bukhari faraz  Hai kHayal aapka, ham nahiN maante !  ","categories": [],
+        "tags": [],
+        "url": "/%E0%A4%B9%E0%A4%AE-%E0%A4%A8%E0%A4%B9%E0%A5%80%E0%A4%82-%E0%A4%AE%E0%A4%BE%E0%A4%A8%E0%A4%A4%E0%A5%87/",
+        "teaser": null
+      },{
+        "title": "Sample",
+        "excerpt":"adfadf  ","categories": [],
+        "tags": [],
+        "url": "/sample/",
+        "teaser": null
+      }]
